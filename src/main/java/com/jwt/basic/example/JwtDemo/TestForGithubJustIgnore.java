395c8054bcdd12ff2d0e560c8cdfa5e4fc1e774a
meesha2000 <52311410+meesha2000@users.noreply.github.com>
2023-02-02 23:11:55 +0530

    New sysout added for test fetch and pull command
    New sysout added for test fetch and pull command. Test ignore

--- src/main/java/com/jwt/basic/example/JwtDemo/TestForGithubJustIgnore.java
+++ src/main/java/com/jwt/basic/example/JwtDemo/TestForGithubJustIgnore.java
@@ -5,6 +5,7 @@ public class TestForGithubJustIgnore {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Test Test Ignore And delete later");
+		System.out.println("Test new 2");
 	}
 
 }
